72f26254e58eb7cdb08f4e1b12304aed0a43e333
Jonathan Winandy <jonathan@univalence.io>
2020-02-18 13:06:10 +0100

    improve formating on Disturb

--- src/test/scala/zio/test/Disturb.scala
+++ src/test/scala/zio/test/Disturb.scala
@@ -1,34 +1,36 @@
 package zio.test
 
 import zio.ZIO.{ EffectTotal, FlatMap, InterruptStatus, Succeed }
-import zio.{ IO, UIO, ZIO }
+import zio.{ IO, ZIO }
 
 object Disturb {
-
-  private def loop[R, E, A](effect: ZIO[R, E, A], remainingStep: Int): ZIO[R, Option[E], (Int, A)] = {
-
-    def next[R1, E1, A1](effect: ZIO[R1, E1, A1]): ZIO[R1, Option[E1], (Int, A1)] = loop(effect, remainingStep - 1)
-
-    def empty: IO[None.type, Nothing] = ZIO.failNow(None)
-
-    def lift[R1, E1, A1](effect: ZIO[R1, E1, A1], steps: Int = 1): ZIO[R1, Option[E1], (Int, A1)] =
-      effect.mapError(x => Option(x)).map((remainingStep - steps) -> _)
-
-    effect match {
-      case e: InterruptStatus[R, E, A] if e.flag.isInterruptible => next(e.zio)
-      case e: InterruptStatus[R, E, A]                           => lift(e) //Will not disturb something not interruptible.
-      case _ if remainingStep <= 0                               => empty
-      case f: FlatMap[R, E, _, A]                                => next(f.zio) >>= { case (n, x) => loop(f.k(x), n) }
-      case e: EffectTotal[A]                                     => lift(e)
-      case e: Succeed[A]                                         => lift(e, steps = 0)
-      //case _ => ??? // implement for other subclasses.
-    }
-  }
 
   /**
    * cut the execution of an effect after n sub operations
    */
   def maxSteps[R, E, A](effect: ZIO[R, E, A], n: Int): ZIO[R, Option[E], A] =
-    loop(effect, n).map(_._2)
+    loop(effect, n).map(_.value)
+
+  private final case class Value[+A](remainingSteps: Int, value: A)
+
+  private def loop[R, E, A](effect: ZIO[R, E, A], remainingSteps: Int): ZIO[R, Option[E], Value[A]] = {
+    def next[R1, E1, A1](effect: ZIO[R1, E1, A1]): ZIO[R1, Option[E1], Value[A1]] = loop(effect, remainingSteps - 1)
+
+    def lift[R1, E1, A1](effect: ZIO[R1, E1, A1]): ZIO[R1, Option[E1], Value[A1]] =
+      effect.asSomeError.map(value => Value(remainingSteps = remainingSteps - 1, value = value))
+
+    effect match {
+      case e: InterruptStatus[R, E, A] if e.flag.isInterruptible => next(e.zio)
+      case e: InterruptStatus[R, E, A]                           => lift(e) //Will not disturb something not interruptible.
+      case _ if remainingSteps <= 0                              => ZIO.failNow(None)
+      case e: EffectTotal[A]                                     => lift(e)
+      case e: Succeed[A]                                         => lift(e)
+      //case _ => ??? // implement for other subclasses.
+      case f: FlatMap[R, E, _, A] =>
+        next(f.zio) >>= { x =>
+          loop(f.k(x.value), x.remainingSteps)
+        }
+    }
+  }
 
 }
